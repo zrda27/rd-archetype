c2479d56f93d5182e932aeb57eb36810480cbed8
zrda27 <zrda27@163.com>
2018-02-25 02:39:41 +0800

    to complete

--- rd-spring-boot-app-archetype/src/main/resources/archetype-resources/service/src/main/java/__packageInPathFormat__/__mainClass__ServiceRunner.java
+++ rd-spring-boot-app-archetype/src/main/resources/archetype-resources/service/src/main/java/__packageInPathFormat__/__mainClass__ServiceRunner.java
@@ -4,10 +4,20 @@
 package ${package};
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-public class ${mainClass}ServiceRunner {
-	
+@SpringBootApplication
+public class ${mainClass}ServiceRunner extends SpringBootServletInitializer  {
+
 	public static void main(String[] args) {
 		SpringApplication.run(${mainClass}ServiceRunner.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(${mainClass}ServiceRunner.class);
+	}
+	
 }
--- rd-spring-boot-app-archetype/src/main/resources/archetype-resources/web/src/main/java/__packageInPathFormat__/__mainClass__WebRunner.java
+++ rd-spring-boot-app-archetype/src/main/resources/archetype-resources/web/src/main/java/__packageInPathFormat__/__mainClass__WebRunner.java
@@ -4,10 +4,20 @@
 package ${package};
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-public class ${mainClass}WebRunner {
-	
+@SpringBootApplication
+public class ${mainClass}WebRunner extends SpringBootServletInitializer  {
+
 	public static void main(String[] args) {
 		SpringApplication.run(${mainClass}WebRunner.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(${mainClass}WebRunner.class);
+	}
+	
 }
